1a3e95c5896a478dc2a7c2b5b520e7f12d7da4ef
deepsinky <deepsinky@gmail.com>
2026-03-24 16:37:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.post("/chat", async (req, res) => {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "llama3-8b-8192",
+          model: "llama3-70b-8192"
           messages: [
             {
               role: "user",
