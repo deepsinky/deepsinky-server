3743a34b5a588b7ed6daef70526859f2c131ee15
deepsinky <deepsinky@gmail.com>
2026-04-18 20:15:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 // ✅ MONGODB CONNECT
-process.env.MONGO_URI && mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 mongoose.connection.on("connected", ()=>{
   console.log("MongoDB Connected ✅");
